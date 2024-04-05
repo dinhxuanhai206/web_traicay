26ea989508ecd84695ad3738a4df1d05993b62c6
dinhxuanhai206 <62803572+dinhxuanhai206@users.noreply.github.com>
2024-04-05 08:40:00 +0000

    put

--- api/controllers/ProductController.js
+++ api/controllers/ProductController.js
@@ -1,37 +1,66 @@
-const productService = require('../services/ProductService');
-const createProduct = async (req, res) => {
-    const newProduct = new Product(req.body);
-    try {
-        const newProduct = await productService.createProduct(productData);
-        res.status(201).json(newProduct);
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
+const productService = require('../services/productService');
+
+async function createProduct(req, res) {
+  try {
+    const newProduct = await productService.createProduct(req.body);
+    res.status(200).json(newProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 }
 
-const getAllproduct = async (req, res) => {
-    try {
-        const products = await productService.getAllProduct()
-        res.json(products);
-    } catch {
-        res.status(500).json({ message: error.message });
-    }
+async function updateProduct(req, res) {
+  try {
+    const updatedProduct = await productService.updateProduct(req.params.id, req.body);
+    res.status(200).json(updatedProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 }
 
-const getProduct = async (req, res) => {
-    try {
-        const product = await productService.getProduct(req.params.id)
-        if (product === null) {
-            return res.status(404).json({ message: 'Product not found' });
-        }
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+async function deleteProduct(req, res) {
+  try {
+    await productService.deleteProduct(req.params.id);
+    res.status(200).json('Product has been deleted...');
+  } catch (err) {
+    res.status(500).json(err);
+  }
 }
 
+async function getProductById(req, res) {
+  try {
+    const product = await productService.getProductById(req.params.id);
+    res.status(200).json(product);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+}
+
+async function getAllProducts(req, res) {
+  try {
+    const qNew = req.query.new;
+    const qCategory = req.query.category;
+
+    let products;
+
+    if (qNew) {
+      products = await productService.getNewProducts();
+    } else if (qCategory) {
+      products = await productService.getProductsByCategory(qCategory);
+    } else {
+      products = await productService.getAllProducts();
+    }
+
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+}
 
 module.exports = {
-    createProduct,
-    getAllproduct,
-    getProduct
-}
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  getProductById,
+  getAllProducts,
+};
--- api/services/ProductService.js
+++ api/services/ProductService.js
@@ -1,19 +1,40 @@
 const Product = require('../models/Product');
-const createProduct = async (productData) => {
-    const product = new Product(productData);
-    return await product.save();
+
+async function createProduct(productData) {
+  const newProduct = new Product(productData);
+  return await newProduct.save();
 }
 
-const getAllProduct = async () => {
-    return await Product.find();
+async function updateProduct(id, productData) {
+  return await Product.findByIdAndUpdate(id, { $set: productData }, { new: true });
 }
 
-const getProduct = async (id) => {
-    return await Product.findById(id)
+async function deleteProduct(id) {
+  return await Product.findByIdAndDelete(id);
+}
+
+async function getProductById(id) {
+  return await Product.findById(id);
+}
+
+async function getAllProducts() {
+  return await Product.find();
+}
+
+async function getNewProducts() {
+  return await Product.find().sort({ createdAt: -1 }).limit(1);
+}
+
+async function getProductsByCategory(category) {
+  return await Product.find({ categories: { $in: [category] } });
 }
 
 module.exports = {
-    createProduct,
-    getAllProduct,
-    getProduct
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  getProductById,
+  getAllProducts,
+  getNewProducts,
+  getProductsByCategory,
 };
